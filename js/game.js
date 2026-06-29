aa65806f4b51acbfa446baccfc59f250c4b34655
MatzeRB26 <m.stojak14@gmail.com>
2026-06-29 10:56:32 +0200

    Create and describe classes

--- js/game.js
+++ js/game.js
@@ -1,6 +1,6 @@
 let canvas; // eine variable für das Bildformat(720x480px)
 let ctx; //ctx steht für context
-let character = new MoveableObject();
+let world = new World();
 
 
 
@@ -9,5 +9,5 @@ function init(){
     ctx = canvas.getContext('2d');
     
     
-    
+    console.log('My Character is', world.character);
 }

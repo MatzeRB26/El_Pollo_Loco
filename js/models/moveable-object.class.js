462f0268d88ef288821a50741baa8e6184c36af1
MatzeRB26 <m.stojak14@gmail.com>
2026-06-30 11:10:47 +0200

    loaded in Pepe's movement via imagecache

--- js/models/moveable-object.class.js
+++ js/models/moveable-object.class.js
@@ -4,10 +4,25 @@ export class MoveableObject {
     img;
     height =  150;
     width = 100;
+    imageCache = {};
 
     loadImage(path){
         this.img = new Image();
         this.img.src = path;
+    }
+
+/** 
+* 
+*@param {Array} arr - ['img/image1.png', 'img/image2.png', ...]
+*/
+
+    loadImages(arr){
+        arr.forEach((path) => {
+
+        let image = new Image();
+        img.src = path;
+        this.imageCache[path] = path;
+        });
     }
 
     moveRight() {

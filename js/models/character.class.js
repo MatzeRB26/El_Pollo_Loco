7add8c54207a7c88479eaff4e2b6ba26a6d92fba
MatzeRB26 <m.stojak14@gmail.com>
2026-07-01 11:39:08 +0200

    Gravity is implemented for when you fall from above

--- js/models/character.class.js
+++ js/models/character.class.js
@@ -1,52 +1,67 @@
 import { MoveableObject } from "./moveable-object.class.js";
 
 export class Character extends MoveableObject {
-    
     height = 280;
-    y = 155;
+    y = 80;
     speed = 10;
     IMAGES_WALKING = [
-        'assets/img/2_character_pepe/2_walk/W-21.png',
-        'assets/img/2_character_pepe/2_walk/W-22.png',
-        'assets/img/2_character_pepe/2_walk/W-23.png',
-        'assets/img/2_character_pepe/2_walk/W-24.png',
-        'assets/img/2_character_pepe/2_walk/W-25.png',
-        'assets/img/2_character_pepe/2_walk/W-26.png'
+        "assets/img/2_character_pepe/2_walk/W-21.png",
+        "assets/img/2_character_pepe/2_walk/W-22.png",
+        "assets/img/2_character_pepe/2_walk/W-23.png",
+        "assets/img/2_character_pepe/2_walk/W-24.png",
+        "assets/img/2_character_pepe/2_walk/W-25.png",
+        "assets/img/2_character_pepe/2_walk/W-26.png",
     ];
+
+    IMAGES_JUMPING = [
+        "assets/img/2_character_pepe/3_jump/J-31.png",
+        "assets/img/2_character_pepe/3_jump/J-32.png",
+        "assets/img/2_character_pepe/3_jump/J-33.png",
+        "assets/img/2_character_pepe/3_jump/J-34.png",
+        "assets/img/2_character_pepe/3_jump/J-35.png",
+        "assets/img/2_character_pepe/3_jump/J-36.png",
+        "assets/img/2_character_pepe/3_jump/J-37.png",
+        "assets/img/2_character_pepe/3_jump/J-38.png",
+        "assets/img/2_character_pepe/3_jump/J-39.png",
+    ];
+
     world;
 
-    constructor(){
-        super().loadImage('assets/img/2_character_pepe/2_walk/W-21.png');
+    constructor() {
+        super().loadImage("assets/img/2_character_pepe/2_walk/W-21.png");
         this.loadImages(this.IMAGES_WALKING);
-
+        this.loadImages(this.IMAGES_JUMPING);
+        this.applyGravity();
         this.animate();
-        }
+    }
 
-    animate(){
-
+    animate() {
         setInterval(() => {
-            if (this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x){
-                    this.x += this.speed;
-                    this.otherDirection = false;
-                    }
+            if (
+                this.world.keyboard.RIGHT &&
+                this.x < this.world.level.level_end_x
+            ) {
+                this.x += this.speed;
+                this.otherDirection = false;
+            }
             if (this.world.keyboard.LEFT && this.x > 0) {
-                    this.x -= this.speed;
-                    this.otherDirection = true;
-                    }
+                this.x -= this.speed;
+                this.otherDirection = true;
+            }
             this.world.camera_x = -this.x + 100;
         }, 1000 / 60);
 
-
-
-    setInterval(() => {
-
-        if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT){
-            this.playAnimation(this.IMAGES_WALKING);
-        }
+        setInterval(() => {
+            if (this.isAboveGround()) {
+                this.playAnimation(this.IMAGES_JUMPING);
+            } else {
+                if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
+                    this.playAnimation(this.IMAGES_WALKING);
+                }
+            }
         }, 50);
     }
-    
-    jump(){
 
-    }
+    
+    jump() {}
 }

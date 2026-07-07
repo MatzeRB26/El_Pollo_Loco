5c642762db26ebed0ce33494b324166ab313a10b
MatzeRB26 <m.stojak14@gmail.com>
2026-07-07 11:03:24 +0200

    feat: Small chickens added, with animations

--- js/models/chicken.class.js
+++ js/models/chicken.class.js
@@ -8,13 +8,13 @@ export class Chicken extends MoveableObject {
     markedForDeletion = false;
 
     IMAGES_WALKING = [
-        "assets/img/3_enemies_chicken/chicken_normal/1_walk/1_w.png",
-        "assets/img/3_enemies_chicken/chicken_normal/1_walk/2_w.png",
-        "assets/img/3_enemies_chicken/chicken_normal/1_walk/3_w.png",
+        'assets/img/3_enemies_chicken/chicken_normal/1_walk/1_w.png',
+        'assets/img/3_enemies_chicken/chicken_normal/1_walk/2_w.png',
+        'assets/img/3_enemies_chicken/chicken_normal/1_walk/3_w.png',
     ];
 
     IMAGE_DEAD = [
-        "assets/img/3_enemies_chicken/chicken_normal/2_dead/dead.png",
+        'assets/img/3_enemies_chicken/chicken_normal/2_dead/dead.png',
     ];
 
     offset = {
